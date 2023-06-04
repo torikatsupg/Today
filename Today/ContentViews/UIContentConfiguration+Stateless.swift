41d01883301bb915376d8fb190c4834e9c4ecbb9
torikatsupg <torikatsu923@gmail.com>
2023-06-04 23:57:48 +0900

    Confirm UIContentView

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by torii katsuya on 2023/06/04.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
